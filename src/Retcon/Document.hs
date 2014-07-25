14972a55421d48f258739d9adbc2ef2639981515
Timo von Holtz <tvh@tvholtz.de>
2014-07-25 13:06:03 +1000

    marshal documents from JSON

--- src/Retcon/Document.hs
+++ src/Retcon/Document.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Retcon.Document where
 
 import           Data.Aeson
@@ -5,6 +7,10 @@ import           Data.Map (Map)
 import qualified Data.Map as M
 import           Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.HashMap.Lazy as H
+
+import Control.Applicative
+import Control.Monad
 
 -- | Nested key/value documents containing 'Text' values.
 data Document = Document (Map Text DocValue)
@@ -17,5 +23,16 @@ data DocValue
     deriving (Show, Eq, Ord)
 
 instance FromJSON Document where
-  parseJSON (Object v) = Ducument 
+  parseJSON (Object v) = do
+    let kvs = H.toList v
+    kvs' <- mapM (\(k,v') -> return . (k,) =<< parseJSON v') kvs
+    return $ Document (M.fromList kvs')
+  parseJSON _ = mzero
 
+instance FromJSON DocValue where
+  parseJSON x@Object{} = Subdocument <$> parseJSON x
+  parseJSON Array{} = mzero
+  parseJSON (String t) = return $ Value t
+  parseJSON (Number n) = return $ Value $ T.pack $ show n
+  parseJSON (Bool b) = return (Value $ T.pack $ show b)
+  parseJSON Null = return $ Value ""
