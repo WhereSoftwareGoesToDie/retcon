4df0b2e47450c2a265cf4187cb73022664eb1fd2
Thomas Sutton <me@thomas-sutton.id.au>
2014-07-25 16:26:03 +1000

    Fix renamed constructor in diff test

--- tests/DiffTest.hs
+++ tests/DiffTest.hs
@@ -6,11 +6,11 @@ import System.Exit
 
 testDiff :: Diff (Int, String)
 testDiff = Diff (1, "hello")
-    [ InsOp (2, "never") ["name"] "Thomas"
-    , InsOp (2, "gonna") ["name"] "Thomas"
-    , InsOp (2, "give") ["name"] "Thomas"
-    , InsOp (2, "you") ["name"] "Thomas"
-    , InsOp (2, "up") ["name"] "Thomas"
+    [ InsertOp (2, "never") ["name"] "Thomas"
+    , InsertOp (2, "gonna") ["name"] "Thomas"
+    , InsertOp (2, "give") ["name"] "Thomas"
+    , InsertOp (2, "you") ["name"] "Thomas"
+    , InsertOp (2, "up") ["name"] "Thomas"
     ]
 
 main :: IO ()
