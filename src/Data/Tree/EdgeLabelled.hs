e1c50b18b9ce3ba95fd03072225402dceaddbb46
Thomas Sutton <me@thomas-sutton.id.au>
2014-07-28 18:03:40 +1000

    Working fromList and toList functions for Tree k v

--- src/Data/Tree/EdgeLabelled.hs
+++ src/Data/Tree/EdgeLabelled.hs
@@ -16,8 +16,38 @@ data Tree key value = Node
 emptyTree :: Tree a b
 emptyTree = Node Nothing M.empty
 
-navigate :: Ord k => [k] -> Tree k v -> Maybe v
+-- | Follow a path and return the value, if any, at the end.
+navigate :: (Ord k)
+         => [k]
+         -> Tree k v
+         -> Maybe v
 navigate []     (Node v _kids) = v
 navigate (n:ns) (Node _ kids ) = M.lookup n kids >>= navigate ns
 
+-- | Convert a 'Tree' into an association list of tree paths and values.
+toList :: (Ord k)
+       => Tree k v
+       -> [([k], v)]
+toList = map (\(k,v) -> (reverse k, v)) . worker []
+  where
+    worker p (Node Nothing  kids) =       children p kids
+    worker p (Node (Just v) kids) = (p,v):children p kids
+    children p = concatMap (\(l,c) -> worker (l:p) c) . M.toList
+
+-- | Convert an association list of tree paths and values into a 'Tree'.
+fromList :: (Ord k)
+         => [([k], v)]
+         -> Tree k v
+fromList [] = emptyTree
+fromList ks = foldl (worker) emptyTree ks
+  where
+    worker (Node l kids) ([]  ,v) = Node (Just v) kids
+    worker (Node l kids) (k:ks,v) = Node l $ M.alter (update (ks,v)) k kids
+    update vs       Nothing = update vs $ Just emptyTree
+    update ([],v)   (Just (Node _ ch)) = Just $ Node (Just v) ch
+    update (k:ks,v) (Just (Node l ch)) =
+      let emptyCase = update (ks,v) $ Just emptyTree
+          fullCase n = update (ks,v) $ Just n
+          ch' = M.alter (maybe emptyCase fullCase) k ch
+      in Just $ Node l ch'
 
