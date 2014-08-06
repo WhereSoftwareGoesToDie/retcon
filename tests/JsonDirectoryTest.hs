a712c567c39e2950201d655d2c50efd3c185b1d0
Geoffrey Roberts <groberts@anchor.net.au>
2014-08-06 17:20:30 +1000

    JsonDirectoryTest now copies entire data tree to /tmp so it can work on it separately

--- tests/JsonDirectoryTest.hs
+++ tests/JsonDirectoryTest.hs
@@ -19,77 +19,74 @@ import System.Directory
 import System.FilePath
 
 import TestHelpers
+import TestScaffold
+
+-- | get source file path
+testSrc :: FilePath
+testSrc = joinPath ["tests", "data"]
+
+-- | get source file path
+testDst :: FilePath
+testDst = joinPath ["/tmp", "tests"]
+
+-- | get temp source file path
+sourceFp :: FilePath
+sourceFp = joinPath ["/tmp", "tests", "data"]
+
+-- | get temp target file path
+targetFp :: FilePath
+targetFp = joinPath ["/tmp", "tests", "test-results"]
 
 suite :: Spec
 suite = do
     describe "JSON directory marshalling" $ do
         it "can load 01-diff-source" $ do
-            f <- sourceFp
-            test1 <- testReadJsonDir f (ForeignKey "01-diff-source")
+            test1 <- testReadJsonDir sourceFp (ForeignKey "01-diff-source")
             either (error "Could not load file") (const pass) test1
 
         it "can load 01-diff-target" $ do
-            f <- sourceFp
-            test2 <- testReadJsonDir f (ForeignKey "01-diff-target")
+            test2 <- testReadJsonDir sourceFp (ForeignKey "01-diff-target")
             either (error "Could not load file") (const pass) test2
 
     describe "JSON directory writing" $ do
         it "can write new file" $ do
-            f  <- sourceFp
-            f' <- targetFp
-            preTest3 <- testReadJsonDir f (ForeignKey "01-diff-source")
+            preTest3 <- testReadJsonDir sourceFp (ForeignKey "01-diff-source")
             case preTest3 of
                 Left err  -> error "Should have got a document but didn't"
                 Right doc -> do
-                    test3 <- testWriteJsonDirNew f' doc
+                    test3 <- testWriteJsonDirNew targetFp doc
                     either (error "Could not write file") (const pass) test3
 
         it "can write existing file" $ do
-            f  <- sourceFp
-            f' <- targetFp
-            preTest4a <- testReadJsonDir f (ForeignKey "01-diff-source")
+            preTest4a <- testReadJsonDir sourceFp (ForeignKey "01-diff-source")
             case preTest4a of
                 Left err  -> error "Should have got a document but didn't"
                 Right doc -> do
-                    preTest4b <- testWriteJsonDirNew f' doc
+                    preTest4b <- testWriteJsonDirNew targetFp doc
                     case preTest4b of
                         Left err        -> error "Couldn't write document when we should have done so"
                         Right (Nothing) -> error "Didn't get a ForeignKey"
                         Right (Just fk) -> do
-                            test4 <- testWriteJsonDirExisting f' doc fk
+                            test4 <- testWriteJsonDirExisting targetFp doc fk
                             either (error "Could not write file again") (const pass) test4
 
     describe "JSON directory deleting" $ do
         it "can delete existing file" $ do
-            f  <- sourceFp
-            f' <- targetFp
-            preTest5a <- testReadJsonDir f (ForeignKey "01-diff-source")
+            preTest5a <- testReadJsonDir sourceFp (ForeignKey "01-diff-source")
             case preTest5a of
                 Left err  -> error "Should have got a document but didn't"
                 Right doc -> do
-                    preTest5b <- testWriteJsonDirNew f' doc
+                    preTest5b <- testWriteJsonDirNew targetFp doc
                     case preTest5b of
                         Left err        -> error "Couldn't write document when we should have done so"
                         Right (Nothing) -> error "Didn't get a ForeignKey"
                         Right (Just fk) -> do
-                            test5 <- testDeleteJsonDir f' fk
+                            test5 <- testDeleteJsonDir targetFp fk
                             either (error "Could not write file again") (const pass) test5
-
--- | get source file path
-sourceFp :: IO FilePath
-sourceFp = do
-    cwd <- getCurrentDirectory
-    return $ joinPath [cwd, "tests", "data"]
-
--- | get target file path
-targetFp :: IO FilePath
-targetFp = do
-    cwd <- getCurrentDirectory
-    return $ joinPath [cwd, "tests", "test-results"]
 
 main :: IO ()
 main = do
-    f' <- targetFp
-    createDirectoryIfMissing True f'
+    preloadTargetDir testSrc testDst
+    createDirectoryIfMissing True targetFp
     hspec suite
 
