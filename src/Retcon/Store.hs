67a5a1dd15c4390e65e566f6b3bc522fec8ea1d7
Thomas Sutton <thomas.sutton@anchor.net.au>
2014-09-29 14:44:06 +1000

    Rework Retcon.Store interface
    - Implement new interface "token" values and type classes.
    
    - Rename operations in existing RetconStore typeclass to make way for
      the new operations (renamed ops will only be used in implementing
      the new ones anyway).

--- src/Retcon/Store.hs
+++ src/Retcon/Store.hs
@@ -7,19 +7,31 @@
 -- the 3-clause BSD licence.
 --
 
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE Rank2Types            #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE Rank2Types                #-}
 
 -- | Description: Abstract storage for retcon operational data.
 --
 -- Retcon maintains quite a lot of operational data. This module defines an
--- interface which can be implemented against various data storage backends.
+-- interface which can be implemented against various data storage backends,
+-- along with types and typeclasses which provide both a restricted and a
+-- permissive interface to this.
 --
 -- The library includes type implementations of this interface in the
 -- "Retcon.Store.PostgreSQL" and "Retcon.Store.Memory" modules.
-module Retcon.Store where
-
-import GHC.TypeLits
+module Retcon.Store (
+    -- * Storage backends
+    RetconStore(..),
+    -- * API
+    StoreToken(..),
+    ReadableToken(..),
+    WritableToken(..),
+    -- * Tokens
+    token,
+    ROToken,
+    RWToken,
+    ) where
 
 import Retcon.DataSource
 import Retcon.Diff
@@ -38,25 +50,138 @@ class RetconStore s where
     -- | Initialise a handle to the storage backend.
     --
     -- (E.g. connect to the database server, etc.)
-    initialiseStorage :: RetconOptions
-                      -> IO s
+    storeInitialise :: RetconOptions
+                    -> IO s
 
     -- | Finalise a handle to the storage backend.
     --
     -- (E.g. disconnect from the database server, etc.)
-    finaliseStorage :: s
-                    -> IO ()
+    storeFinalise :: s
+                  -> IO ()
 
     -- | Allocate and return a new 'InternalKey'.
-    createInternalKey :: forall entity. (RetconEntity entity)
-                      => s
-                      -> IO (InternalKey entity)
+    storeCreateInternalKey :: forall entity. (RetconEntity entity)
+                           => s
+                           -> IO (InternalKey entity)
 
+    -- | Find the 'InternalKey' associated with a 'ForeignKey'.
+    storeLookupInternalKey :: (RetconDataSource e d)
+                           => s
+                           -> ForeignKey e d
+                           -> IO (Maybe (InternalKey e))
+
+    -- | Delete an 'InternalKey' and any associated resources.
+    storeDeleteInternalKey :: (RetconEntity entity)
+                           => s
+                           -> InternalKey entity
+                           -> IO ()
+
+    -- | Record a 'ForeignKey' and it's association with an 'InternalKey'.
+    storeRecordForeignKey :: (RetconDataSource e d)
+                          => s
+                          -> InternalKey e
+                          -> ForeignKey e d
+                          -> IO ()
+
+    -- | Delete a 'ForeignKey'.
+    storeDeleteForeignKey :: (RetconDataSource e d)
+                          => s
+                          -> ForeignKey e d
+                          -> IO ()
+
+    -- | Delete all 'ForeignKey's associated with an 'InternalKey'.
+    storeDeleteForeignKeys :: (RetconEntity e)
+                           => s
+                           -> InternalKey e
+                           -> IO ()
+
+    -- | Find the 'ForeignKey' corresponding to an 'InternalKey' in a particular
+    -- data source.
+    storeLookupForeignKey :: (RetconDataSource e d)
+                          => s
+                          -> InternalKey e
+                          -> IO (Maybe (ForeignKey e d))
+
+    -- | Record the initial 'Document' associated with an 'InternalKey'.
+    storeRecordInitialDocument :: (RetconEntity e)
+                               => s
+                               -> InternalKey e
+                               -> Document
+                               -> IO ()
+
+    -- | Lookup the initial 'Document', if any, associated with an 'InternalKey'.
+    storeLookupInitialDocument :: (RetconEntity e)
+                               => s
+                               -> InternalKey e
+                               -> IO (Maybe Document)
+
+    -- | Delete the initial 'Document', if any, associated with an 'InternalKey'.
+    storeDeleteInitialDocument :: (RetconEntity e)
+                               => s
+                               -> InternalKey e
+                               -> IO ()
+
+    -- | Record the success 'Diff' and a list of failed 'Diff's associated with a
+    -- processed 'InternalKey'.
+    storeRecordDiffs :: (RetconEntity e)
+                     => s
+                     -> InternalKey e
+                     -> (Diff l, [Diff l])
+                     -> IO ()
+
+    -- | Delete the 'Diff's associated with an 'InternalKey'.
+    storeDeleteDiffs :: (RetconEntity e)
+                     => s
+                     -> InternalKey e
+                     -> IO Int
+
+-- * Tokens
+
+-- $ Tokens wrap storage backend values and expose particular subsets of the
+-- complete storage interface to client code.
+
+-- | Wrap a storage backend value in a token.
+token :: (RetconStore s)
+      => s
+      -> RWToken
+token = RWToken
+
+-- | Storage tokens expose an APIs to the underlying storage backend.
+--
+-- Each token will carry instances of one or more other typeclasses
+-- ('ReadableToken', 'WritableToken') which define the operations permitted by
+-- that token type.
+class StoreToken s where
+    -- | Restrict a token to be read-only.
+    restrictToken :: s -> ROToken
+
+-- | Storage tokens which support reading operations.
+class StoreToken s => ReadableToken s where
     -- | Find the 'InternalKey' associated with a 'ForeignKey'.
     lookupInternalKey :: (RetconDataSource e d)
                       => s
                       -> ForeignKey e d
                       -> IO (Maybe (InternalKey e))
+
+    -- | Find the 'ForeignKey' corresponding to an 'InternalKey' in a particular
+    -- data source.
+    lookupForeignKey :: (RetconDataSource e d)
+                     => s
+                     -> InternalKey e
+                     -> IO (Maybe (ForeignKey e d))
+
+    -- | Lookup the initial 'Document', if any, associated with an 'InternalKey'.
+    lookupInitialDocument :: (RetconEntity e)
+                          => s
+                          -> InternalKey e
+                          -> IO (Maybe Document)
+
+-- | Storage tokens which support writing operations.
+class StoreToken s => WritableToken s where
+    -- | Allocate and return a new 'InternalKey'.
+    createInternalKey :: forall entity. (RetconEntity entity)
+                      => s
+                      -> IO (InternalKey entity)
 
     -- | Delete an 'InternalKey' and any associated resources.
     deleteInternalKey :: (RetconEntity entity)
@@ -79,16 +204,9 @@ class RetconStore s where
 
     -- | Delete all 'ForeignKey's associated with an 'InternalKey'.
     deleteForeignKeys :: (RetconEntity e)
-                     => s
-                     -> InternalKey e
-                     -> IO ()
-
-    -- | Find the 'ForeignKey' corresponding to an 'InternalKey' in a particular
-    -- data source.
-    lookupForeignKey :: (RetconDataSource e d)
-                     => s
-                     -> InternalKey e
-                     -> IO (Maybe (ForeignKey e d))
+                      => s
+                      -> InternalKey e
+                      -> IO ()
 
     -- | Record the initial 'Document' associated with an 'InternalKey'.
     recordInitialDocument :: (RetconEntity e)
@@ -96,12 +214,6 @@ class RetconStore s where
                           -> InternalKey e
                           -> Document
                           -> IO ()
-
-    -- | Lookup the initial 'Document', if any, associated with an 'InternalKey'.
-    lookupInitialDocument :: (RetconEntity e)
-                          => s
-                          -> InternalKey e
-                          -> IO (Maybe Document)
 
     -- | Delete the initial 'Document', if any, associated with an 'InternalKey'.
     deleteInitialDocument :: (RetconEntity e)
@@ -122,4 +234,22 @@ class RetconStore s where
                 => s
                 -> InternalKey e
                 -> IO Int
+
+-- | A token exposing only the 'ReadableToken' API.
+data ROToken = forall s. RetconStore s => ROToken s
+
+instance StoreToken ROToken where
+    restrictToken = id
+
+instance ReadableToken ROToken where
+
+-- | A token exposing both the 'ReadableToken' and 'WritableToken' APIs.
+data RWToken = forall s. RetconStore s => RWToken s
+
+instance StoreToken RWToken where
+    restrictToken (RWToken st) = ROToken st
+
+instance ReadableToken RWToken where
+
+instance WritableToken RWToken where
 
