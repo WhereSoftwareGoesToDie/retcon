5f0985fd8c2af4b3fb764f281b45f47c4309a672
Thomas Sutton <thomas.sutton@anchor.net.au>
2014-10-20 16:58:38 +1100

    Start stubbing out API server code

--- lib/Retcon/Network/Server.hs
+++ lib/Retcon/Network/Server.hs
@@ -10,8 +10,46 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
--- | Server-side API implementation.
+-- | Server component for the retcon network API.
 module Retcon.Network.Server where
 
+import Control.Applicative
+import Control.Lens.Operators
+import Control.Lens.TH
+import Data.Monoid
+import Data.String
+import Options.Applicative
+
+import Retcon.Core
 import Retcon.Network.WireFormat
+import Retcon.Options
+
+-- | Configuration for the server.
+data ServerConfig = ServerConfig
+    { _cfgConnectionString :: String
+    }
+  deriving (Show, Eq)
+makeLenses ''ServerConfig
+
+-- | Parser for server options.
+serverParser :: Parser ServerConfig
+serverParser = ServerConfig <$> connString
+  where
+    connString = option str (
+           long "address"
+        <> short 'A'
+        <> metavar "SOCKET"
+        <> help "Server socket. e.g. tcp://0.0.0.0:60179")
+
+-- | Start a server running the retcon API over a ZMQ socket.
+runAPIServer
+    :: WritableToken store
+    => RetconConfig InitialisedEntity store
+    -> ServerConfig
+    -> IO ()
+runAPIServer retconCfg serverCfg = do
+    putStrLn . fromString $
+        "Running server on " <> serverCfg ^. cfgConnectionString 
