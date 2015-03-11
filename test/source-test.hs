4c0c994f7e14059110b6bb4e3076b95cb19398de
Timo von Holtz <tvh@anchor.com.au>
2015-03-11 12:24:08 +1100

    Test for multiple create/read events

--- test/source-test.hs
+++ test/source-test.hs
@@ -10,7 +10,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad
 import Data.Aeson
+import Data.Monoid
 import Test.Hspec
 import Test.HUnit
 
@@ -27,6 +29,12 @@ source = DataSource
     , commandDelete = "mkdir -p entity/source && rm entity/source/%fk.json"
     }
 
+allDifferent :: (Eq a, Show a) => [a] -> Assertion
+allDifferent [] = return ()
+allDifferent (x:xs) = do
+  when (x `elem` xs) $ assertFailure $ "allDifferent: found multiple occurances of " <> show x
+  allDifferent xs
+
 suite :: Spec
 suite = do
     describe "DataSource" $ do
@@ -40,6 +48,17 @@ suite = do
             case res of
                 Left err -> assertFailure (show err)
                 Right doc' -> assertBool "Read returned different object than Created" (doc == doc')
+        it "can create and read out again (multiple times)" $ do
+            let docs = [Document "entity" "source" (object ["num" .= n]) | n <- [1..10::Int]]
+            fks' <- mapM (runDSMonad . createDocument source ) docs
+            fks <- forM fks' $ \fk' -> case fk' of
+                Left err -> assertFailure (show err) >>= undefined
+                Right fk -> return fk
+            allDifferent fks
+            ress <- mapM (runDSMonad . readDocument source) fks
+            forM_ (zip ress docs) $ \res -> case res of
+                (Left err, _) -> assertFailure (show err)
+                (Right doc', doc) -> assertBool "Read returned different object than Created" (doc == doc')
 
 main :: IO ()
 main = hspec suite
